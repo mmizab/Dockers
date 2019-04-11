f81679231fda0a04a138e5920fad5d83cd2e1784
mmizab <mohammed.mizab.latrache@gmail.com>
2019-04-11 19:38:40 +0200

    add script to install docker on ubuntu 18.04 64 bits

---
+++ docker-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+sudo apt update
+sudo apt-get install docker-ce -y
+sudo usermod -aG docker $USER
